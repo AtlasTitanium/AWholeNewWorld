ac2622528873cbb3c036f1b4af6d4f4db359f9a1
PepijnKok <AtlasTheTitan77@gmail.com>
2018-05-15 09:16:01 +0200

    idk

---
+++ AWholeNewWorld/Assets/ScaleSphere.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleSphere : MonoBehaviour {
+
+	public float speed;
+	
+	private float scroll;
+	
+	// Update is called once per frame
+	void Update () {
+		scroll = Input.GetAxis("Mouse ScrollWheel");
+		if(Input.GetMouseButtonDown(0)){
+			Debug.Log("ye");
+			this.transform.localScale = new Vector3(this.transform.localScale.x + speed, this.transform.localScale.y + speed, this.transform.localScale.z + speed);
+		}
+		 if (scroll > 0f){
+			this.transform.localScale = new Vector3(this.transform.localScale.x + speed/2, this.transform.localScale.y + speed/2, this.transform.localScale.z + speed/2);
+		}
+		else if (scroll < 0f){
+			this.transform.localScale = new Vector3(this.transform.localScale.x - speed/2, this.transform.localScale.y - speed/2, this.transform.localScale.z - speed/2);
+		}
+		if(Input.GetMouseButtonDown(1)){
+			Debug.Log("no");
+			this.transform.localScale = new Vector3(1,1,1);
+		}
+	}
+}
---
+++ AWholeNewWorld/Assets/Sonar.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sonar : MonoBehaviour {
+	private Camera Camera;
+	public float speed;
+	private float near;
+	private bool go = false;
+	private bool rern = false;
+	private bool clickable = true;
+	// Use this for initialization
+	void Start () {
+		Camera = GetComponent<Camera>();
+		near = 82f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(go){
+			near += speed;
+			if(near >= 159.3){
+				near = 159.3f;
+				go = false;
+				clickable = true;
+			}
+		} 
+
+		if(rern){
+			near -= speed;
+			if(near <= 82){
+				near = 82f;	
+				rern = false;
+				clickable = true;
+			}
+		} 
+
+		if(clickable){
+			if(Input.GetMouseButtonDown(0)){
+				go = true;
+				clickable = false;
+			}
+			if(Input.GetMouseButtonDown(1)){
+				rern = true;
+				clickable = false;
+			}
+		}
+
+		Camera.nearClipPlane = near;
+	}
+}
